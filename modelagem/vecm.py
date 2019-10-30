c6df6cb6699dd2ea165b4a793ed07603d9af20f9
phelipe <phelipe_teles@hotmail.com>
2019-10-30 19:58:10 -0300

    modelo com ibc -- results estranhos, sem autocorr

--- modelagem/vecm.py
+++ modelagem/vecm.py
@@ -1,21 +1,24 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import statsmodels.api as sm
 from statsmodels.tsa.api import VECM
-from statsmodels.tsa.vector_ar.vecm import select_coint_rank, coint_johansen, select_order
+from statsmodels.tsa.vector_ar.vecm import select_coint_rank, select_order
 
-series = pd.read_csv("../dados/series_log.csv", parse_dates=[0]).set_index("date")
+series = (pd.read_csv("../dados/series_log.csv", parse_dates=[0])
+          .set_index("date")
+          .dropna())
 
 series.plot(subplots=True, layout=(3, 3))
 
 endog = ["spread", "selic", "inad", "prod_ind"]
-endogenous_variables = series.loc[:, endog].dropna()
+endog_vars = series.loc[:, endog]
+
+if "ihh" in endog_vars:
+    series = series.query('date.dt.year <= 2017')
 
 ##### Order Selection #####
 
 order_selection = select_order(
-    endogenous_variables,
+    endog_vars,
     maxlags=12,
     deterministic="colo")
 print(order_selection.summary())
@@ -23,8 +26,8 @@ print(order_selection.summary())
 ##### Cointegration rank test #####
 
 coint_rank = select_coint_rank(
-    endog=endogenous_variables,
-    k_ar_diff=2,
+    endog=endog_vars,
+    k_ar_diff=12,
     det_order=1,
     signif=0.05,
     method="maxeig")
@@ -32,9 +35,8 @@ print(coint_rank)
 
 ##### VECM estimação  #####
 
-vecm_results = VECM(endog=endogenous_variables,
+vecm_results = VECM(endog=endog_vars,
                     k_ar_diff=2,
-                    exog=series.igp[:-1],
                     coint_rank=1,
                     deterministic="colo").fit()
 for i in range(1, 13):
@@ -42,13 +44,10 @@ for i in range(1, 13):
 
 #### Resultados ####
 
-vecm_results.summary()
-
-##### Diagnósticos #####
-
+# vecm_results.summary()
 
 ##### Impulso resposta #####
 
-vecm_irf = vecm_results.irf(12)
-vecm_irf.plot()
-plt.show()
+# vecm_irf = vecm_results.irf(12)
+# vecm_irf.plot()
+# plt.show()
