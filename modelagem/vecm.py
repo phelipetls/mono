c41b875522f4153a195df8d79f768ffeff770729
phelipe <phelipe_teles@hotmail.com>
2019-10-29 21:43:53 -0300

    único modelo até agr sem autocorrelação residual

--- modelagem/vecm.py
+++ modelagem/vecm.py
@@ -5,50 +5,50 @@ import statsmodels.api as sm
 from statsmodels.tsa.api import VECM
 from statsmodels.tsa.vector_ar.vecm import select_coint_rank, coint_johansen, select_order
 
-series = pd.read_csv("../dados/series_log.csv").drop("date", axis="columns")
+series = pd.read_csv("../dados/series_log.csv", parse_dates=[0]).set_index("date")
 
-series.columns
+series.plot(subplots=True, layout=(3, 3))
 
-endog = ["spread", "selic", "inad", "ihh", "pib_mensal"]
-endogenous_variables = series.loc[:, endog]
+endog = ["spread", "selic", "inad", "prod_ind"]
+endogenous_variables = series.loc[:, endog].dropna()
 
 ##### Order Selection #####
 
 order_selection = select_order(
     endogenous_variables,
-    maxlags=3,
+    maxlags=12,
     deterministic="colo")
-
 print(order_selection.summary())
 
 ##### Cointegration rank test #####
 
 coint_rank = select_coint_rank(
     endog=endogenous_variables,
-    k_ar_diff=3,
+    k_ar_diff=2,
     det_order=1,
     signif=0.05,
     method="maxeig")
-
 print(coint_rank)
 
 ##### VECM estimação  #####
 
-vecm = VECM(endog=endogenous_variables, k_ar_diff=3,
-            exog=series.igp,
-            coint_rank=1, deterministic="colo")
+vecm_results = VECM(endog=endogenous_variables,
+                    k_ar_diff=2,
+                    exog=series.igp[:-1],
+                    coint_rank=1,
+                    deterministic="colo").fit()
+for i in range(1, 13):
+    print(vecm_results.test_whiteness(i).summary())
 
-vecm_results = vecm.fit()
+#### Resultados ####
+
+vecm_results.summary()
 
 ##### Diagnósticos #####
 
-print(vecm_results.test_whiteness())
 
 ##### Impulso resposta #####
 
 vecm_irf = vecm_results.irf(12)
 vecm_irf.plot()
 plt.show()
-
-vecm_results.summary()
-
